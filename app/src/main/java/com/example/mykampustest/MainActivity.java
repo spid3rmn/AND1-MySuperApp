5e40fe64f29a76320d15e0e2a603c7ecda9b517a
Francesco Palma <fp484839@gmail.com>
2021-11-16 14:44:47 +0100

    Login done but need to fix some logic

--- app/src/main/java/com/example/mykampustest/MainActivity.java
+++ app/src/main/java/com/example/mykampustest/MainActivity.java
@@ -5,24 +5,24 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
+import android.widget.Toast;
 
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
-
 import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.mykampustest.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
+    private MainActivityViewModel viewModel;
     //private NavController navController;
 
     @Override
@@ -31,6 +31,10 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        //retrieving and initializing viewmodel
+        viewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
+        //viewModel.init();
 
         //floating button
         setSupportActionBar(binding.appBarMain.toolbar);
@@ -75,14 +79,39 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_signin:
+                startLoginActivity();
                 return true;
             case R.id.action_signout:
+                signOut();
                 return true;
             case R.id.action_faq:
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    //my version with no message and toast
+    private void checkIfSignedIn() {
+        viewModel.getCurrentUser().observe(this, user -> {
+            if (user != null) {
+                String message_log = "Welcome " + user.getDisplayName();
+                Toast.makeText(this, message_log, Toast.LENGTH_LONG).show();
+            } else {
+                String message_not = "Hey, who are you?";
+                Toast.makeText(this, message_not, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void startLoginActivity() {
+        startActivity(new Intent(this, SignInActivity.class));
+        finish();
+    }
+
+    //view in the arguments?
+    public void signOut() {
+        viewModel.signOut();
     }
 
     @Override
---
+++ app/src/main/java/com/example/mykampustest/MainActivityViewModel.java
@@ -0,0 +1,41 @@
+package com.example.mykampustest;
+
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.example.mykampustest.data.UserRepository;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivityViewModel extends AndroidViewModel {
+    private final UserRepository userRepository;
+    //private final MessageRepository messageRepository;
+
+    public MainActivityViewModel(Application app){
+        super(app);
+        userRepository = UserRepository.getInstance(app);
+        //messageRepository = MessageRepository.getInstance();
+    }
+
+    public void init() {
+        String userId = userRepository.getCurrentUser().getValue().getUid();
+        //messageRepository.init(userId);
+    }
+
+    public LiveData<FirebaseUser> getCurrentUser(){
+        return userRepository.getCurrentUser();
+    }
+
+    /*public void saveMessage(String message) {
+        messageRepository.saveMessage(message);
+    }*/
+
+    /*public LiveData<Message> getMessage() {
+        return messageRepository.getMessage();
+    }*/
+
+    public void signOut() {
+        userRepository.signOut();
+    }
+}
